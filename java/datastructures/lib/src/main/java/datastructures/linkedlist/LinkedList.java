a6c2f138885840520c95304884c99e4aa62908fa
Lolunts <armasconi@gmail.com>
2022-10-28 18:03:04 -0700

    sll1-first 3  methods-tested

--- java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
+++ java/datastructures/lib/src/main/java/datastructures/linkedlist/LinkedList.java
@@ -2,26 +2,37 @@ package datastructures.linkedlist;
 
 public class LinkedList<T> {
 
-    public Node<T> head = null;
+  public Node<T> head = null;
 
-    public void insert(T val) {
-      Node<T> newHead = new Node<T>(val);
-      newHead.next = head;
-      head = newHead;
-    }
+  public void insert(T val) {
+    Node<T> newHead = new Node<T>(val);
+    newHead.next = head;
+    head = newHead;
+  }
 
-    @Override
-    public String toString() {
-      String res = "";
-      Node<T> cur = head;
-      while (cur != null) {
-        res += "{ " + cur.val + " } -> ";
-        cur = cur.next;
-
+  public boolean includes(T val) {
+    Node<T> cur = head;
+    while (cur != null) {
+      if (cur.val == val) {
+        System.out.println("found"+cur.val);
+        return true;
       }
-      return res + "NULL";
+      cur = cur.next;
     }
+    return false;
+  }
 
+  @Override
+  public String toString() {
+    String res = "";
+    Node<T> cur = head;
+    while (cur != null) {
+      res += "{ " + cur.val + " } -> ";
+      cur = cur.next;
+
+    }
+    return res + "NULL";
+  }
 
 
 }
--- java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
+++ java/datastructures/lib/src/test/java/datastructures/linkedlist/LinkedListTest.java
@@ -4,27 +4,37 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class LinkedListTest
-{
-  @Test void testInsertAsHead()
-    {
-      LinkedList sut = new LinkedList();
-      sut.insert(11);
-      sut.insert(13);
-      int head = (int) sut.head.val;
-      System.out.println(head);
-      assertTrue(head==13);
-    }
+public class LinkedListTest {
+  @Test
+  void testInsertAsHead() {
+    LinkedList sut = new LinkedList();
+    sut.insert(11);
+    sut.insert(13);
+    int head = (int) sut.head.val;
+    System.out.println(head);
+    assertTrue(head == 13);
+  }
 
-  @Test void testSllAsString()
-  {
+  @Test
+  void testSllIncludesVal() {
+    LinkedList sut = new LinkedList();
+    sut.insert(11);
+    sut.insert(13);
+    sut.insert("Papa");
+    assert (sut.includes("Mama") == false);
+    assert (sut.includes("Papa") == true);
+    assert (sut.includes(11) && !sut.includes(7));
+  }
+
+  @Test
+  void testSllAsString() {
     LinkedList sut = new LinkedList();
     sut.insert(11);
     sut.insert(13);
     String test = sut.toString();
     System.out.println(test);
     assertTrue(test.equals(
-      "{ 13 } -> { 11 } -> NULL"
+        "{ 13 } -> { 11 } -> NULL"
       )
     );
   }
