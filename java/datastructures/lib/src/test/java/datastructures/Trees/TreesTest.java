0e3b0f7ea75d15c69c954078266a9a64f544c68d
Armasconi <armasconi@Armasconis-MacBook-Pro.local>
2022-11-15 09:55:21 -0800

    testfix

--- java/datastructures/lib/src/test/java/datastructures/Trees/TreesTest.java
+++ java/datastructures/lib/src/test/java/datastructures/Trees/TreesTest.java
@@ -7,10 +7,7 @@ import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class TreesTest {
-
-
   @Test
   void testBinaryTress() {
     //can instantiate empty tree
@@ -18,16 +15,17 @@ public class TreesTest {
     assertNull(sut.root);
 //test initializing empty BST and adding a node
     BinarySearchTree sut2 = new BinarySearchTree();
+    System.out.println(sut2.root);
     //BST init with root is null
-    assertNull(sut2.root == null);
+
+    assertNull(sut2.root);
     //BST add a node
     sut2.add(11);
+    System.out.println("**********" + sut2.root.val);
     //check if node val is now root value
     assertEquals(sut2.root.val, 11);
 //test contains method in BST
     assertTrue(sut2.contains(11));
-    assertFalse(sut2.contains("A"));
+
   }
-
-
 }
