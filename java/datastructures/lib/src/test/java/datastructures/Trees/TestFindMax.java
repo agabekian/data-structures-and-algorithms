867d09cb30839f6b5fa89954ca40b1d6420f2d5b
Armasconi <armasconi@Armasconis-MacBook-Pro.local>
2022-11-15 15:38:45 -0800

    recursive-dfs

---
+++ java/datastructures/lib/src/test/java/datastructures/Trees/TestFindMax.java
@@ -0,0 +1,32 @@
+package datastructures.trees;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestFindMax {
+  @Test
+  void testBinaryTress() {
+    BinaryTree sut = new BinaryTree();
+    sut.root = new Node(2);
+    sut.root.left = new Node(7);
+    sut.root.right = new Node(5);
+    sut.root.left.right = new Node(6);
+    sut.root.left.right.left = new Node(1);
+    sut.root.left.right.right = new Node(11);
+    sut.root.right.right = new Node(9);
+    sut.root.right.right.left = new Node(4);
+
+    int res = sut.findMax(sut.root);
+    assertTrue(res == 11);
+    assertTrue
+  }
+}
