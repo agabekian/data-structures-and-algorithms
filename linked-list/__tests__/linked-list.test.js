7dc5a9092f9654d0384d4e74314b81df3c6390ba
Armen Agabekian <2008nv@gmail.com>
2024-05-09 08:46:49 -0700

    test

--- linked-list/__tests__/linked-list.test.js
+++ linked-list/__tests__/linked-list.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Node = require('../SLLNode.js');
-const LinkedList = require('code-challenges/linked-list-zip/zipLists');
-const zipLists = require('code-challenges/linked-list-zip/zipLists.js');
+const LinkedList = require('../LinkedList');
+const zipLists = require('../../../data-structures-and-algorithms/code-challenges/linked-list-zip/zipLists');
 // Require our linked list implementation
 describe('LinkedList', () => {
     it('checking if test file works', () => expect(true).toBeTruthy());
@@ -12,7 +12,7 @@ describe('LinkedList', () => {
         expect(linkedList.head).toEqual(null);
     });
 
-    //###### CODE CH 6 ##########
+    //###### CC 06 ##########
     //append method test add node to the end of the list
     it('should append value ', () => {
         const ll = new LinkedList();
@@ -39,9 +39,11 @@ describe('LinkedList', () => {
         ll.append(2);
         ll.append(3);
         ll.insertBefore(2,33)
-           expect(ll.head.next.val).toEqual(33)  //after 2
+        expect(ll.head.next.val).toEqual(33)  //after 2
         && expect(ll.head.next.next.val).toEqual(3); // and before 3
     })
+
+    // ############## CC08 ###################
     it('should have a merged "zipped" list after zip ', () => {
         const l1 = new LinkedList();
         l1.append(1);
@@ -54,19 +56,14 @@ describe('LinkedList', () => {
         l2.append(33);
 
         const expected = new LinkedList();
-        l2.append(1);
-        l2.append(11);
-        l2.append(2);
-        l2.append(22);
-        l2.append(3);
-        l2.append(33);
+        expected.append(1);
+        expected.append(11);
+        expected.append(2);
+        expected.append(22);
+        expected.append(3);
+        expected.append(33);
 
-        let res = zipLists(l1,l2)
-        expect(res).toEqual(Expected)  //after 2
-
+        const result = zipLists(l1,l2)
+        expect(result).toEqual(expected)  //after 2
     })
-
 });
-// it('should return the head of the list', () => {
-
-// ... other tests
