5aa82f626bd14b84e5d6fa011ff98d47c472a4f0
Armen Agabekian <2008nv@gmail.com>
2024-05-10 14:11:38 -0700

    zippety

--- code-challenges/linked-list-zip/zipLists.js
+++ code-challenges/linked-list-zip/zipLists.js
@@ -1,6 +1,6 @@
 
 
-export function zipLists(list1, list2) {
+function zipLists(list1, list2) {
     if (!list1 && !list2) return null;
     if (!list1 || !list2) {
         return list1 || list2;
@@ -13,7 +13,7 @@ export function zipLists(list1, list2) {
             next2 = cur2.next; //
         // console.log("pointers", next1.val,next2.val)
         cur1.next = cur2; //zip to list2   11->
-                                            // 101->808->909
+        // 101->808->909
         // console.log("cur1=head", cur1, "now point to list2 ",cur1.next)
         if (next1) { // is next stored null ?
             cur2.next = next1; // 101 --> 8
@@ -27,35 +27,47 @@ export function zipLists(list1, list2) {
 
 //for tests
 
-let node1 = new Node(11);
-let node2 = new Node(8);
-
-let node3 = new Node(101);
-let node4 = new Node(808);
-let node5 = new Node(909);
-node1.next = node2;
-node3.next = node4;
-node4.next = node5;
-
+// let node1 = new Node(11);
+// let node2 = new Node(8);
+//
+// let node3 = new Node(101);
+// let node4 = new Node(808);
+// let node5 = new Node(909);
+// node1.next = node2;
+// node3.next = node4;
+// node4.next = node5;
+//
 let l1 = new LinkedList();
-let l2 = new LinkedList();
-l1.insert(node1);
-l2.insert(node3);
-
-
-
-const mergedList = zip(l1, l2); //expect 11,101,8,808,13,null
-
-let currentNode = mergedList.head;
-while (currentNode) {
-    console.log(currentNode.val);
-    currentNode = currentNode.next;
+// let l2 = new LinkedList();
+l1.insert(1);
+l1.insert(3);
+l1.insert(1);
+//
+//
+//
+// const mergedList = zip(l1, l2); //expect 11,101,8,808,13,null
+//
+// let currentNode = mergedList.head;
+// while (currentNode) {
+//     console.log(currentNode.val);
+//     currentNode = currentNode.next;
+// }
+function isPali(head) {
+    let slow = head;
+    let stack = [];
+    while (slow != null) {
+        stack.push(slow.val);
+        slow = slow.next;
+    }
+    while (head != null) {
+        let cell = stack.pop();
+        if (cell != head.val) {
+            return false;
+        }
+        head = head.next;
+    }
+    return true;
 }
-// testChain.insert(node1);
-// let nodeToIns = new Node(13);
-// testChain.insertBefore(8, nodeToIns);
-// testChain.toString()
-// let nodeToInsAfter = new Node(999)
-// testChain.insertAfter(11, nodeToInsAfter)
-// console.log(testChain.toString());
+
+console.log(isPali(l1))
 
