1d0641a6e980c3b827dc77e9199bcc1ed22c6fe8
Armen Agabekian <2008nv@gmail.com>
2024-07-03 18:27:27 -0700

    air-itineary-rebuild-7-3-24-interview

---
+++ code-challenges/finalInterview.js
@@ -0,0 +1,54 @@
+const {Stack} = require("../stack-and-queue/stack-and-queue");
+
+//woo that was hard!
+function organizeTickets(stack) {
+    let end = "";
+    let ORIGINS = [];
+    let DESTINATIONS = [];
+    let map = new Map();
+
+    // track origins and destinations
+    while (stack.peek()) {
+        let cur = stack.pop();
+        ORIGINS.push(cur.origin);
+        DESTINATIONS.push(cur.dest);
+        map.set(cur.dest, cur.origin); //had to reverse
+    }
+
+    // Find the destination
+    for (let d of DESTINATIONS) {
+        if (!ORIGINS.includes(d)) {
+            end = d;
+            console.log("Found the FINAL destination: ", end);
+            break;
+        }
+    }
+    let res = new Stack();
+    // Rebuild the order, cool stuff:
+    while (map.has(end)) {
+        let origin = map.get(end); //map is "reversed", since we'll push this into stack
+        res.push({"origin": origin, "dest": end});
+        end = origin; //sweet neat trick
+    }
+    return res;
+}
+
+let t1 = {"origin": "SEA", "dest": "CHI"};
+let t2 = {"origin": "CHI", "dest": "JFK"};
+let t3 = {"origin": "SNO", "dest": "SEA"};
+let t4 = {"origin": "JFK", "dest": "PAR"};
+let stack = new Stack();
+
+stack.push(t1);
+stack.push(t2);
+stack.push(t3);
+stack.push(t4);
+
+let orderedStack = organizeTickets(stack);
+
+// Print the resulting ordered stack
+while (orderedStack.peek()) {
+    console.log(orderedStack.pop());
+}
+
+// ----> O(N)
--- tree/compareTrees.js
+++ tree/compareTrees.js
@@ -5,23 +5,16 @@ class TreeNode {
         this.right = null;
     }
 }
-
 function areIdenticalTrees(root1, root2) {
-    // Base case: if both roots are null, they are identical
-    if (root1 === null && root2 === null) {
-        return true;
-    }
-
+    if (root1 === null && root2 === null) return true;
     // If one root is null and the other is not, they are not identical
-    if (root1 === null || root2 === null) {
-        return false;
-    }
+    if (root1 === null || root2 === null) return false;
+    // Check if current nodes have the same value
+    if (root1.value !== root2.value) return false; //boom!.. exit
 
-    // Check if current nodes have the same value and recursively check left and right subtrees
+    // Recursively check left and right subtrees
     return (
-        root1.value === root2.value &&
-        areIdenticalTrees(root1.left, root2.left) &&
-        areIdenticalTrees(root1.right, root2.right)
+        areIdenticalTrees(root1.left, root2.left) && areIdenticalTrees(root1.right, root2.right)
     );
 }
 
@@ -40,4 +33,6 @@ tree2.left.left = new TreeNode(4);
 tree2.left.right = new TreeNode(5);
 
 // Comparing the trees
-console.log(areIdenticalTrees(tree1, tree2)); // Output: true
+console.log(
+    areIdenticalTrees(tree1, tree2)
+); // Output: true
